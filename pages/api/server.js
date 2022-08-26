f81f54ad97c4805c7ee1993ce7ec980d80afc1ca
omar chatin <omerchatin19@gmail.com>
2022-08-26 03:39:52 +0300

    w

--- pages/api/server.js
+++ pages/api/server.js
@@ -1,15 +1,13 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-// const token = process.env.TELEGRAM_BOT;
-const token = "5580783584:AAEDuAolkA0v2dPonhJxZyQ7V6QUAHh_CCk";
-const bot = new TelegramBot(token, { polling: true });
-
-
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Received your message");
-});
-
 export default function handler(req, res) {
+  const token = "5580783584:AAEDuAolkA0v2dPonhJxZyQ7V6QUAHh_CCk";
+  const bot = new TelegramBot(token, { polling: true });
+
+  bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Received your message");
+  });
+
   res.status(200).json({ name: "Bot is live!" });
 }
